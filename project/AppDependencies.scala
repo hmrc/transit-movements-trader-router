61d5951e359e396d31525a0331419bd6cb261076
GeorgeColdham <georgecoldham@hotmail.co.uk>
2021-02-05 14:23:39 +0000

    updated dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"        % "3.2.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"        % "3.3.0"
   )
 
   val test = Seq(
@@ -15,7 +15,7 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.2"                 % "test, it",
     "org.mockito"             %  "mockito-all"              % "1.10.19"               % "test, it",
     "org.scalacheck"          %% "scalacheck"               % "1.14.0"                % "test, it",
     "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.25.0"                % "test, it"
