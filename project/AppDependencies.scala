30cc93f996a0fd40d5b8cb1bc6a24763a99e1d52
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2021-08-19 15:23:19 +0100

    CLI-71 Update dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,21 +5,19 @@ import sbt._
 
 object AppDependencies {
 
-
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"        % "3.3.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "5.12.0"
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.2.8"                 % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.scalatestplus"       %% "scalacheck-1-15"          % "3.2.9.0"               % "test, it",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it",
-    "org.scalatestplus"      %% "mockito-3-2"               % "3.1.2.0"               % "test, it",
-    "org.mockito"             % "mockito-core"              % "3.9.0"                 % "test, it",
-    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.25.0"                % "test, it",
-    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10"               % "test, it"
-
+    "org.scalatest"          %% "scalatest"           % "3.2.9"   % "test",
+    "com.typesafe.play"      %% "play-test"           % current   % "test",
+    "org.scalatestplus"      %% "scalacheck-1-15"     % "3.2.9.0" % "test, it",
+    "org.pegdown"             % "pegdown"             % "1.6.0"   % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"  % "4.0.3"   % "test, it",
+    "org.scalatestplus"      %% "mockito-3-2"         % "3.1.2.0" % "test, it",
+    "org.mockito"             % "mockito-core"        % "3.9.0"   % "test, it",
+    "com.github.tomakehurst"  % "wiremock-standalone" % "2.25.0"  % "test, it",
+    "com.vladsch.flexmark"    % "flexmark-all"        % "0.35.10" % "test, it"
   )
 }
